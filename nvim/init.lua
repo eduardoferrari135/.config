65fd5f0330e6e891643cf4d9a6d2547d0c9ef4ac
eduardoferrari135 <eduardoferrari135@gmail.com>
2024-02-27 22:57:22 -0300

    update: gruvbox theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -202,7 +202,7 @@ require('lazy').setup({
         opts = {
             options = {
                 icons_enabled = false,
-                theme = 'catppuccin',
+                theme = 'gruvbox',
                 component_separators = '|',
                 section_separators = '',
             },
@@ -273,7 +273,7 @@ vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 
 require("catppuccin").setup()
 
-vim.cmd([[colorscheme catppuccin]])
+vim.cmd([[colorscheme gruvbox]])
 
 vim.o.relativenumber = true
 
--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -15,20 +15,35 @@
 # export BG1=0xff363944
 # export BG2=0xff414550
 
-### Catppuccin
-export BLACK=0xff181926
-export WHITE=0xffcad3f5
-export RED=0xffed8796
-export GREEN=0xffa6da95
-export BLUE=0xff8aadf4
-export YELLOW=0xffeed49f
-export ORANGE=0xfff5a97f
-export MAGENTA=0xffc6a0f6
-export GREY=0xff939ab7
+### Gruvbox
+export BLACK=0xff282828
+export WHITE=0xfffbf1c7
+export RED=0xfffb4934
+export GREEN=0xffb8bb26
+export BLUE=0xff83a598
+export YELLOW=0xfffabd2f
+export ORANGE=0xfffe8019
+export MAGENTA=0xffd3869b
+export GREY=0xff928374
 export TRANSPARENT=0x00000000
-export BG0=0xff1e1e2e
-export BG1=0x603c3e4f
-export BG2=0x60494d64
+export BG0=0xff282828
+export BG1=0xff3c3836
+export BG2=0xff504945
+
+### Catppuccin
+# export BLACK=0xff181926
+# export WHITE=0xffcad3f5
+# export RED=0xffed8796
+# export GREEN=0xffa6da95
+# export BLUE=0xff8aadf4
+# export YELLOW=0xffeed49f
+# export ORANGE=0xfff5a97f
+# export MAGENTA=0xffc6a0f6
+# export GREY=0xff939ab7
+# export TRANSPARENT=0x00000000
+# export BG0=0xff1e1e2e
+# export BG1=0x603c3e4f
+# export BG2=0x60494d64
 
 # General bar colors
 export BAR_COLOR=$BG0
