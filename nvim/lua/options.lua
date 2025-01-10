1235bb25f228589d210dd6d41666aaca37578966
eduardoferrari135 <eduardoferrari135@gmail.com>
2025-01-10 19:58:04 -0300

    delete: ndash

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,11 +4,3 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
-vim.api.nvim_create_autocmd("BufDelete", {
-  callback = function()
-    local bufs = vim.t.bufs
-    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
-      vim.cmd "Nvdash"
-    end
-  end,
-})
